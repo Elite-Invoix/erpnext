22f8509b2b03c0667464eb3695c7d1f71b33bc25
Rahul Mishra <rahul.mishra605@gmail.com>
2025-07-16 17:50:07 +0000

    uncomment code

--- erpnext/accounts/doctype/payment_entry/payment_entry.js
+++ erpnext/accounts/doctype/payment_entry/payment_entry.js
@@ -444,25 +444,25 @@ frappe.ui.form.on("Payment Entry", {
 		frm.refresh_fields();
 	},
 
-	// show_general_ledger: function (frm) {
-	// 	if (frm.doc.docstatus > 0) {
-	// 		frm.add_custom_button(
-	// 			__("Ledger"),
-	// 			function () {
-	// 				frappe.route_options = {
-	// 					voucher_no: frm.doc.name,
-	// 					from_date: frm.doc.posting_date,
-	// 					to_date: moment(frm.doc.modified).format("YYYY-MM-DD"),
-	// 					company: frm.doc.company,
-	// 					group_by: "",
-	// 					show_cancelled_entries: frm.doc.docstatus === 2,
-	// 				};
-	// 				frappe.set_route("query-report", "General Ledger");
-	// 			},
-	// 			"fa fa-table"
-	// 		);
-	// 	}
-	// },
+	show_general_ledger: function (frm) {
+		// if (frm.doc.docstatus > 0) {
+		// 	frm.add_custom_button(
+		// 		__("Ledger"),
+		// 		function () {
+		// 			frappe.route_options = {
+		// 				voucher_no: frm.doc.name,
+		// 				from_date: frm.doc.posting_date,
+		// 				to_date: moment(frm.doc.modified).format("YYYY-MM-DD"),
+		// 				company: frm.doc.company,
+		// 				group_by: "",
+		// 				show_cancelled_entries: frm.doc.docstatus === 2,
+		// 			};
+		// 			frappe.set_route("query-report", "General Ledger");
+		// 		},
+		// 		"fa fa-table"
+		// 	);
+		// }
+	},
 
 	payment_type: function (frm) {
 		if (frm.doc.payment_type == "Internal Transfer") {
